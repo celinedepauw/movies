3c385face6bab3240f4f14bea1cb83c1dceabdb2
Celine Depauw <celinefauchier@hotmail.com>
2020-12-30 16:50:09 +0100

    liens catégories ok sur la page home

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -14,7 +14,15 @@
         <div class="card border-0 text-light text-center"><img src="<?= $categories[0]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100 py-3">
-              <a href="#">
+              <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[0]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[0]['category_name']; ?></h2>
               </a>
             </div>
@@ -25,7 +33,15 @@
         <div class="card border-0 text-light text-center"><img src="<?= $categories[1]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100 py-3">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[1]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[1]['category_name']; ?></h2>
               </a>
             </div>
@@ -39,7 +55,15 @@
         <div class="card border-0 text-center text-light"><img src="<?= $categories[2]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[2]['id']
+          ]
+          );
+              ?>
+              <a href=""<?=$url;?>>
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[2]['category_name']; ?></h2>
               </a>
             </div>
@@ -51,7 +75,15 @@
           <img src="<?= $categories[3]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[3]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[3]['category_name']; ?></h2>
               </a>
             </div>
@@ -64,7 +96,15 @@
         <div class="card border-0 text-center text-light"><img src="<?= $categories[4]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[4]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[4]['category_name']; ?></h2>
               </a>
             </div>
@@ -75,7 +115,15 @@
         <div class="card border-0 text-center text-light"><img src="<?= $categories[5]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[5]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[5]['category_name']; ?></h2>
               </a>
             </div>
@@ -88,7 +136,15 @@
         <div class="card border-0 text-center text-light"><img src="<?= $categories[6]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[6]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[6]['category_name']; ?></h2>
               </a>
             </div>
@@ -99,7 +155,15 @@
         <div class="card border-0 text-center text-light"><img src="<?= $categories[7]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
-              <a href="category.html">
+            <?php
+              $url = $router->generate(
+                'category',
+          [
+            'idCategory' => $categories[7]['id']
+          ]
+          );
+              ?>
+              <a href="<?=$url;?>">
                 <h2 class="display-3 font-weight-bold mb-4"><?= $categories[7]['category_name']; ?></h2>
               </a>
             </div>
