1e50a629fe0a6155393b9db47cd6b070c6655b83
Celine Depauw <celinefauchier@hotmail.com>
2021-02-02 18:25:59 +0100

    page home dynamique

--- app/views/home.tpl.php
+++ app/views/home.tpl.php
@@ -4,216 +4,38 @@
 
 //foreach($categories as $index => $category){
 //echo $categories[$index]['category_name'];
-// echo '<br>';
+//echo '<br>';
 //}
 ?>
+
+
 <section>
   <div class="container-fluid">
 
-    <div class="row mx-0">
-      <div class="col-md-6">
-        <div class="card border-0 text-light text-center"><img src="<?= $categories[0]['category_picture']; ?>" alt="Card image" class="card-img">
+<?php
+  foreach($categories as $index => $category):
+?> 
+
+      <div class="movie-category">
+        <div class="card border-0 text-light text-center"><img src="<?= $categories[$index]['category_picture']; ?>" alt="Card image" class="card-img">
           <div class="card-img-overlay d-flex align-items-center">
             <div class="w-100">
               <?php
               $url = $router->generate(
                 'category',
           [
-            'idCategory' => $categories[0]['id']
+            'idCategory' => $categories[$index]['id']
           ]
           );
               ?>
               <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[0]['category_name']; ?></h2>
+                <h2 class="category-name display-3 font-weight-bold mb-4 p-1"><?= $categories[$index]['category_name']; ?></h2>
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-6">
-        <div class="card border-0 text-light text-center"><img src="<?= $categories[1]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[1]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[1]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mx-0">
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[2]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[2]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[2]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light">
-          <img src="<?= $categories[3]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[3]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[3]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mx-0">
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[4]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[4]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[4]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-black"><img src="<?= $categories[5]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[5]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[5]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mx-0">
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[6]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[6]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[6]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[7]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[7]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[7]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="row mx-0">
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[8]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[8]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[8]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card border-0 text-center text-light"><img src="<?= $categories[9]['category_picture']; ?>" alt="Card image" class="card-img">
-          <div class="card-img-overlay d-flex align-items-center">
-            <div class="w-100">
-            <?php
-              $url = $router->generate(
-                'category',
-          [
-            'idCategory' => $categories[9]['id']
-          ]
-          );
-              ?>
-              <a href="<?=$url;?>">
-                <h2 class="display-3 font-weight-bold mb-4 p-1"><?= $categories[9]['category_name']; ?></h2>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
+        <?php endforeach;?>   
     </div>             
     </div>
 
